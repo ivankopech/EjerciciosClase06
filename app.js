eda1e7703a89872af7b06a5cc8c1524261e1d288
ivan kopech <ivankopech@gmail.com>
2021-05-22 13:55:28 -0300

    if

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('ok');
+//1 Completar las condiciones de los if del siguiente script para que los mensajes de los
+//alert se muestren de forma correcta.
+// var num1 = 5;
+// var num2 = 8;
+
+// if((num1 - num2) <= 0)
+// {
+//     alert('num1 no es mayor que num2');
+// }
+
+// if((num1 + num2) > num1) 
+// {
+//     alert('num2 es positivo');
+// }
+
+// if((num1 * num2) != 0) 
+// {
+//     alert('num1 es negativo o distinto de cero');
+// }
+
+// if(num2 - 3 == num1 ) 
+// {
+//     alert('Incrementar en 1 unidad el valor de num1 lo hace mayor que num2');
+// }
